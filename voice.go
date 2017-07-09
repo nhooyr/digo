935fabd1e7c20acd0830e5a40f8292bcad527be5
Anmol Sethi <anmol@aubble.com>
2017-07-09 12:17:27 -0400

    Add Voice Resource

---
+++ voice.go
@@ -0,0 +1,26 @@
+package digo
+
+import "github.com/bwmarrin/snowflake"
+
+type VoiceState struct {
+	GuildID   *snowflake.ID
+	ChannelID snowflake.ID
+	UserID    snowflake.ID
+	SessionID string
+	Deaf      bool
+	Mute      bool
+	SelfDeaf  bool
+	SelfMute  bool
+	Suppress  bool
+}
+
+type VoiceRegion struct {
+	ID             string
+	Name           string
+	SampleHostname string
+	SamplePort     int
+	VIP            bool
+	Optimal        bool
+	Deprecated     bool
+	Custom         bool
+}
