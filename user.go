94698223871f0a0b488ddc9116136d9e231a8b66
Anmol Sethi <anmol@aubble.com>
2017-07-09 12:17:20 -0400

    Add User Resource

---
+++ user.go
@@ -0,0 +1,30 @@
+package digo
+
+import "github.com/bwmarrin/snowflake"
+
+type User struct {
+	ID            snowflake.ID
+	Username      string
+	Discriminator string
+	Avatar        string
+	Bot           bool
+	MFAEnabled    bool
+	Verified      bool
+	Email         string
+}
+
+type UserGuild struct {
+	ID          snowflake.ID
+	Name        string
+	Icon        string
+	Owner       bool
+	Permissions int
+}
+
+type Connection struct {
+	ID string
+	Name string
+	Types string
+	Revoked bool
+	Integrations []*Integration
+}
