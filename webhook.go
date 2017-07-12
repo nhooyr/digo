38485b7cd7c4ef044d246c60100f23f29f027006
Anmol Sethi <anmol@aubble.com>
2017-07-12 12:51:34 -0400

    TODO webhook, gonna polish the REST API now.

--- webhook.go
+++ webhook.go
@@ -1,11 +1,12 @@
 package discgo
 
+// TODO not needed for now, but maybe later?
 type Webhook struct {
-	ID        string
-	GuildID   *string
-	ChannelID string
-	User      *User
-	Name      *string
-	Avatar    *string
-	Token     string
+	ID        string  `json:"id"`
+	GuildID   *string `json:"guild_id"`
+	ChannelID string  `json:"channel_id"`
+	User      *User   `json:"user"`
+	Name      *string `json:"name"`
+	Avatar    *string `json:"avatar"`
+	Token     string  `json:"token"`
 }
