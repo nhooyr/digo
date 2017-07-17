32e2c20f9b15aa9c5bea5db2349eb2784716f3f6
Anmol Sethi <anmol@aubble.com>
2017-07-17 17:16:04 -0400

    new TODOs

--- docs.go
+++ docs.go
@@ -2,6 +2,8 @@
 Provide a low level interface to the Discord API but also maintain clean readable code.
 TODO think about the license
 TODO need any nulls?
+TODO update names
+TODO update test names
 
 RestAPI does not use One Methods e.g. Channels().One(cID) because too much stutter.
 RestAPI does not have all methods defined on single Client because too little structure and hard code completion/doc finding
