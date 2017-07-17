29c62c7c5ae160874f3fb10ed9507896728aba9f
Anmol Sethi <anmol@aubble.com>
2017-07-17 16:26:33 -0400

    done invite_test.go

--- invite_test.go
+++ invite_test.go
@@ -5,7 +5,7 @@ import "testing"
 var inviteCode = "NP9NQ8v"
 
 func TestClient_GetInvite(t *testing.T) {
-	inv, err := c.GetInvite(inviteCode)
+	inv, err := c.Invite(inviteCode).Get()
 	if err != nil {
 		t.Fatal(err)
 	}
