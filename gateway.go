d3ba1ef5f1beeece5b6ef2d9b9a1e109e1544e86
Anmol Sethi <anmol@aubble.com>
2017-07-20 23:22:31 -0400

    fixed small formatting issue

--- gateway.go
+++ gateway.go
@@ -78,7 +78,7 @@ func NewConn(apiClient *Client) (*Conn, error) {
 }
 
 const (
-	dispatchOperation            = iota
+	dispatchOperation = iota
 	heartbeatOperation
 	identifyOperation
 	statusUpdateOperation
